0823568fb79f56d26778e50ed5ebad5cb1609d70
Giorgos Bitiloulis <bitgiorgos@gmail.com>
2023-03-16 21:25:33 +0200

    First express endpoint

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-fetch("https://www.white-pages.gr/arithmos/6983030304").then( r => console.log(r))
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json("Hello, I'm Kamikaizi")
+})
+
+app.listen(5000, () => console.log("server running on port 5000"))
